492491849eaba5eecc5264b03d2eecfa52eb573b
Anykiev Alexey <anikevaleksej92@gmail.com>
2021-04-09 17:42:38 +0300

    Перенос Numbers

---
+++ AjaxRequest/Models/Numbers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AjaxRequest
+{
+    public class Numbers
+    {
+        
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
